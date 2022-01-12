d46d146e18d388786cb321be544810d522e0f2b1
Ilya Zabolotin <mcclen@rambler.ru>
2022-01-12 19:25:46 +0300

    fix: write a code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+  if (n < 0){
+      n = -n;
+  }
+  let s = String(n);
+  let arrS = [];
+  arrS = s.split('');
+  let arrNS = [];
+  arrNS = arrS.reverse();
+  let reverseS = arrNS.join('');
+  let newN = Number(reverseS);
+  return newN;
 }
+
+
